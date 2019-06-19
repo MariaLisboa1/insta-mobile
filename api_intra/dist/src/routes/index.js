e0a68a6222b1158517b19c1d1eaa500ae993805e
Maria <32674641+MariaLisboa1@users.noreply.github.com>
2019-06-19 11:23:46 -0300

    Delete index.js

--- api_intra/dist/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-let Router = express.Router();
-//ROTAS
-exports.default = Router;
