0ddc3184e136f69212cf7b006cbe68ee7bb963e0
zaza.naza95 <zaza.naza95@gmail.com>
2019-06-19 11:20:59 -0300

    projeto app do instagram com a tecnlogia react-native

--- insta/src/pages/Feed.js
+++ insta/src/pages/Feed.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import io from 'socket.io-client';
 import { Text, Image, View, StyleSheet, TouchableOpacity, FlatList } from 'react-native'
+
 import camera from '../assets/camera2.png';
+import like from '../assets/like.png';
+import more from '../assets/more.png';
+import send from '../assets/send.png';
+import comment from '../assets/comment.png';
+
 import api from '../services/api';
 
 export default class Feed extends Component {
@@ -17,38 +24,139 @@ export default class Feed extends Component {
     };
 
     async componentDidMount() {
-        //this.registerToSocket();
-        console.log('response')
+        this.registerToSocket();
+       
         const response = await api.get('posts');
-      
-        
+
         this.setState({ feed: response.data });
     }
 
+    
+    registerToSocket = () => {
+        const socket = io('http://10.0.4.70:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] }) //fazendo real time
+        })
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post    
+                )
+            })
+        })
+    }
+
+    
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
+    }
+
     render() {
-        return ( 
-            <View style={styles.container}>
+        return (
+            <View style={styles.container }>
                 <FlatList
-                data={this.state.feed}
-                keyExtractor={post => post._id}
-                renderItem={({ item })=> (
-                    <View style={styles.feedItem}>
-                       
-                        <View style={styles.feedItemHeader}>
-                            <View style={styles.userInfor}>
-                                <Text style={styles.name}>{item.author}</Text>
-                                <Text style={styles.place}>{item.place}</Text>
+                    data={this.state.feed}
+                    keyExtractor={post => post._id}
+                    renderItem={({ item }) => (
+                        <View style={styles.feedItem}>
+
+                            <View style={styles.feedItemHeader}>
+                                <View style={styles.userInfor}>
+                                    <Text style={styles.name}>{item.author}</Text>
+                                    <Text style={styles.place}>{item.place}</Text>
+                                </View>
+
+                                <Image source={more} />
+                            </View>
+
+                            <Image style={styles.feedImage} source={{ uri: `http://10.0.4.70:3333/files/${item.image}` }} />
+                            <View style={styles.feedItemFooter}>
+                                <View style={styles.actions}>
+                                    <TouchableOpacity style={styles.action} onPress={() => this.handleLike(item._id)}>
+                                        <Image source={like} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.action} onPress={() => { }}>
+                                        <Image source={comment} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.action} onPress={() => { }}>
+                                        <Image source={send} />
+                                    </TouchableOpacity>
+                                </View>
+
+                                <Text style={styles.likes}>{item.likes} curtidas</Text>
+                                <Text style={styles.description}>{item.description}</Text>
+                                <Text style={styles.hashtags}>{item.hashtags}</Text>
                             </View>
                         </View>
-
-                    </View>
-                )}
+                    )}
                 />
             </View>
         )
     }
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+    },
+
+    feedItem: {
+        marginTop: 20
+    },
+
+    feedItemHeader: {
+        paddingHorizontal: 15,
+
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    name: {
+        fontSize: 14,
+        color: '#000',
+    },
+
+    place: {
+        fontSize: 12,
+        color: '#666',
+        marginTop: 2,
+    },
+
+    feedImage: {
+        width: '100%',
+        height: 400,
+        marginVertical: 15,
+    },
+
+    feedItemFooter: {
+        paddingHorizontal: 15,
+    },
+
+    actions: {
+        flexDirection: 'row',
+    },
+
+    action: {
+        marginRight: 8,
+    },
+
+    likes: {
+        marginTop: 15,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+
+    description: {
+        lineHeight: 18,
+        color: '#000',
+    },
+
+    hashtags: {
+        color: '#7159c1'
+    }
 
 });
